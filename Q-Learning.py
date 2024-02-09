64603415728e62d5923602a52295e9ad7eb52647
Jivan Kesan <117189726+jivankesan@users.noreply.github.com>
2024-02-09 15:47:34 -0500

    Update Q-Learning.py

--- Q-Learning.py
+++ Q-Learning.py
@@ -35,24 +35,60 @@ class MemoryReplay(object):
 
 
 class Dqn():
-    def __init__(self, input_size=3, action=1, gamma=0.99, epsilon=1.0, epsilon_decay=0.995, epsilon_min=0.01):
+    
+    def __init__(self, input_size, nb_action, gamma):
         self.gamma = gamma
         self.reward_window = []
-        self.model = NeuralNetwork.Network(input_size, action)
-        self.memory = MemoryReplay(100000)
-        self.optimizer = optim.Adam((self.model.parameters()), lr = 0.01) #can experiment with multiple optimizers
-        self.prev_state = nn.Tensor(input_size).unsqueeze(0)
-        self.prev_action = 0 #0 is for straight, (0,-1) is left, (1,0) is right
-        self.prev_reward = 0
-
+        self.model = Network(input_size, nb_action)
+        self.memory = ReplayMemory(100000)
+        self.optimizer = optim.Adam(self.model.parameters(), lr = 0.001)
+        self.last_state = torch.Tensor(input_size).unsqueeze(0)
+        self.last_action = 0
+        self.last_reward = 0
+    
     def select_action(self, state):
-        probs = f.softmax(self.model(Variable(state, volatile=True))*20) #temperature parameter = 7
-        action = probs.multinomial()
-        return action.data[0, 0]
-
-    def learn(self, batch_state, batch_next, batch_reward, batch_action):
-
-
-
-
-# add optimizer for SGD (using Adam)
+        probs = F.softmax(self.model(Variable(state, volatile = True))*100) # T=100
+        action = probs.multinomial(num_samples=1)
+        return action.data[0,0]
+    
+    def learn(self, batch_state, batch_next_state, batch_reward, batch_action):
+        outputs = self.model(batch_state).gather(1, batch_action.unsqueeze(1)).squeeze(1)
+        next_outputs = self.model(batch_next_state).detach().max(1)[0]
+        target = self.gamma*next_outputs + batch_reward
+        td_loss = F.smooth_l1_loss(outputs, target)
+        self.optimizer.zero_grad()
+        td_loss.backward(retain_graph = True)
+        self.optimizer.step()
+    
+    def update(self, reward, new_signal):
+        new_state = torch.Tensor(new_signal).float().unsqueeze(0)
+        self.memory.push((self.last_state, new_state, torch.LongTensor([int(self.last_action)]), torch.Tensor([self.last_reward])))
+        action = self.select_action(new_state)
+        if len(self.memory.memory) > 100:
+            batch_state, batch_next_state, batch_action, batch_reward = self.memory.sample(100)
+            self.learn(batch_state, batch_next_state, batch_reward, batch_action)
+        self.last_action = action
+        self.last_state = new_state
+        self.last_reward = reward
+        self.reward_window.append(reward)
+        if len(self.reward_window) > 1000:
+            del self.reward_window[0]
+        return action
+    
+    def score(self):
+        return sum(self.reward_window)/(len(self.reward_window)+1.)
+    
+    def save(self):
+        torch.save({'state_dict': self.model.state_dict(),
+                    'optimizer' : self.optimizer.state_dict(),
+                   }, 'last_brain.pth')
+    
+    def load(self):
+        if os.path.isfile('last_brain.pth'):
+            print("=> loading checkpoint... ")
+            checkpoint = torch.load('last_brain.pth')
+            self.model.load_state_dict(checkpoint['state_dict'])
+            self.optimizer.load_state_dict(checkpoint['optimizer'])
+            print("done !")
+        else:
+            print("no checkpoint found...")
